97e9374fd722b9b7b6856c5d6582215860be6651
Dhruvi K <dkunvarani@gmail.com>
2024-08-11 15:22:17 -0400

    part 2 sending email via aws completed

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const axios = require("axios");
+const AWS = require("aws-sdk");
 
+// Configuration
 const MONDAY_API_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM5NTUzMDgyMSwiYWFpIjoxMSwidWlkIjo2NDYxODMxNSwiaWFkIjoiMjAyNC0wOC0xMVQwNDo1MDoxNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjQ4NjM1MzUsInJnbiI6InVzZTEifQ.OAiEqv2kfD6HgjBDUA1LJjTnmfRoeQbFACrFs7GDGp4";
 const MONDAY_BOARD_ID = "7202199016";
 
-// Part 1 Function to fetch data from Monday.com
+AWS.config.update({ region: "us-east-1" });
+const ses = new AWS.SES({ apiVersion: "2010-12-01" });
+
+// Part 1: Function to fetch data from Monday.com
 async function fetchMondayBoardData() {
   const query = `
     query {
@@ -43,16 +48,17 @@ async function fetchMondayBoardData() {
       const items = boards[0].items_page.items || {};
       if (items.length !== 0) {
         for (let i = 0; i < items.length; i++) {
-          let obj = {}
+          let obj = {};
           obj.name = items[i].name;
-          for(let j = 0; j< items[i].column_values.length; j++){
-            obj[`${items[i].column_values[j].id}`] = items[i].column_values[j].value
+          for (let j = 0; j < items[i].column_values.length; j++) {
+            obj[`${items[i].column_values[j].id}`] =
+              items[i].column_values[j].value;
           }
           result.push(obj);
         }
       }
     }
-    return result
+    return result;
   } catch (error) {
     console.error(
       "Error fetching data:",
@@ -62,13 +68,50 @@ async function fetchMondayBoardData() {
   }
 }
 
-// Main function to execute the Part 1 & Part 2
+// Part 2: Function to send an email using AWS SES
+async function sendEmail(toEmail, content) {
+  const params = {
+    Destination: {
+      ToAddresses: [toEmail],
+    },
+    Message: {
+      Body: {
+        Text: { Data: content },
+      },
+      Subject: { Data: "Feedback Email" },
+    },
+    Source: "dkunvarani@gmail.com",
+  };
+
+  try {
+    await ses.sendEmail(params).promise();
+    console.log(`Email sent to ${toEmail}`);
+  } catch (error) {
+    console.error(`Error sending email to ${toEmail}:`, error.message);
+    throw error;
+  }
+}
+
+const getEmail = (data) => {
+  const JSONData = JSON.parse(data)
+  return JSONData.email || ''
+}
+
+// Main function to execute the steps
 async function main() {
   try {
-    // Step 1: Fetch data from Monday.com
+    // Part 1: Fetch data from Monday.com
     const boardData = await fetchMondayBoardData();
 
-    console.log(boardData);
+    // Part 2: Send emails
+    for (const entry of boardData) {
+      const email = getEmail(entry["client_email"]);
+      const emailContent = entry["email_content"];
+
+      if (email != '' && emailContent) {
+        await sendEmail(email, emailContent);
+      }
+    }
   } catch (error) {
     console.error("An error occurred:", error.message);
   }
